7ead0e83fb7fbfd9864f8a9d2bdc5cc37e397fc7
Eklavaya Singh <eklavya.singh7933@gmail.com>
2026-01-10 22:54:42 +0530

    1st array Question

--- Arrays/Largest element in an array.py
+++ Arrays/Largest element in an array.py
@@ -1,2 +1,11 @@
 #Solution
 
+def largest(nums):
+    largest= nums[0]
+    n = len(nums)
+    for i in range(0,n):
+        largest = max(largest,nums[i])
+    return largest
+
+n = [3,4,1]
+print(largest(n))
