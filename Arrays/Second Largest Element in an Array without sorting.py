feca6a3c2a83191ecf2cd02418d3e7c238140ba8
Eklavaya Singh <eklavya.singh7933@gmail.com>
2026-01-11 22:21:04 +0530

    array question

--- Arrays/Second Largest Element in an Array without sorting.py
+++ Arrays/Second Largest Element in an Array without sorting.py
@@ -1,11 +1,16 @@
 #Solution
 
-def largest(nums):
-    largest= nums[0]
+def second_large(nums):
+    largest = float("-inf")
+    S_largest = float("-inf")
     n = len(nums)
     for i in range(0,n):
         largest = max(largest,nums[i])
-    return largest
+    for i in range(0,n):
+        if nums[i]>S_largest and nums[i]!=largest:
+            S_largest = nums[i]
+    return S_largest
+    
 
-n = [3,4,1]
-print(largest(n))
+n = [3,4,1,7,9,10]
+print(second_large(n))
