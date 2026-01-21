242f3b8d29b254075ee716f2d78b7ab040cd1251
Eklavaya Singh <eklavya.singh7933@gmail.com>
2026-01-21 23:30:14 +0530

    array question

---
+++ Arrays/max consecutive ones.py
@@ -0,0 +1,15 @@
+#Solution
+
+def maxconsectiveOnes(nums):
+    count = 0
+    max_count = 0
+    for i in range(0,len(nums)):
+        if nums[i] == 1:
+            count+=1
+        else:
+            max_count = max(max_count,count)
+            count = 0
+    return max(max_count,count)
+
+n = [1,1,0,1,1,0,1,1,1,1,0,1,1,1,1,1,1]
+print(maxconsectiveOnes(n))
