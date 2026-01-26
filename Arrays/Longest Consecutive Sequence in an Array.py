3556d909f2c8d2d2588b0242ffc0b496dd708138
Eklavaya Singh <eklavya.singh7933@gmail.com>
2026-01-26 21:58:14 +0530

    array question

---
+++ Arrays/Longest Consecutive Sequence in an Array.py
@@ -0,0 +1,20 @@
+#Solution
+
+def longest_consecutive(nums):
+    n = len(nums)
+    my_set = set()
+    for i in range(0,n):
+        my_set.add(nums[i])
+    longest = 0
+    for num in my_set:
+        if num-1 not in my_set:
+            x = num
+            count = 1
+            while x+1 in my_set:
+                count+=1
+                x+=1
+            longest = max(longest,count)
+    return longest
+
+nums = [100,4,200,1,3,2]
+print(longest_consecutive(nums))
