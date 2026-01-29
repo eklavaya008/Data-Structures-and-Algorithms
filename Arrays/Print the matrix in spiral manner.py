16bb679a388d4239ec0af5c5dce7e3761c197714
Eklavaya Singh <eklavya.singh7933@gmail.com>
2026-01-29 22:10:41 +0530

    array question

---
+++ Arrays/Print the matrix in spiral manner.py
@@ -0,0 +1,28 @@
+#Soltion
+
+def spiralOrder(matrix):
+    if not matrix or not matrix[0]:
+        result = []
+    result = []
+    top , left = 0,0
+    bottom = len(matrix) -1 
+    right = len(matrix[0]) - 1
+    while top <= bottom and left <=right:
+        for i in range(left , right+1):
+            result.append(matrix[top][i])
+        top+=1
+        for i in range(top , bottom+1):
+            result.append(matrix[i][right])
+        right-=1
+        if top <= bottom:
+            for i in range(right, left-1,-1):
+                result.append(matrix[bottom][i])
+            bottom -=1
+        if left <= right:
+            for i in range(bottom,top-1,-1):
+                result.append(matrix[i][left])
+            left +=1
+    return result
+
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+print(spiralOrder(matrix))
