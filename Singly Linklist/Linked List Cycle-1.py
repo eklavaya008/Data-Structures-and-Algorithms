858902f6961bdccdfa54688fe7c5834d4066ba11
Eklavaya Singh <eklavya.singh7933@gmail.com>
2026-02-28 23:04:42 +0530

    Singly Linklist question

---
+++ Singly Linklist/Linked List Cycle-1.py
@@ -0,0 +1,16 @@
+#Solution
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+def LinkedLCycle1(head):
+    slow = head
+    fast = head
+    while fast != None and fast.next!=None:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
