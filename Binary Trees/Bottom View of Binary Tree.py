7a5350767243e29b78f81930be8e91306367df87
Eklavaya Singh <eklavya.singh7933@gmail.com>
2026-06-11 22:54:59 +0530

    binary tree question

---
+++ Binary Trees/Bottom View of Binary Tree.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.right = None
+        self.left = None
+from collections import deque
+class Solution:
+    def topView(self, root):
+        if root is None:
+            return 0
+        ans = []
+        queue = deque()
+        result = {}
+        queue.append((root,0))
+        while queue:
+            e , line = queue.popleft()
+            result[line] = e.data
+            if e.left:
+                queue.append((e.left,line-1))
+            if e.right:
+                queue.append((e.right,line+1))
+        for value in sorted(result.items()):
+            ans.append(value[1])
+        return ans
+
