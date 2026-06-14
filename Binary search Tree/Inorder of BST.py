e1183a179f2b247d86ece3712e330d08b31b6124
Eklavaya Singh <eklavya.singh7933@gmail.com>
2026-06-14 23:52:37 +0530

    BST question

---
+++ Binary search Tree/Inorder of BST.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isBSTTraversal(self, arr):
+        n = len(arr)
+        
+        for i in range(1,n):
+            if arr[i] <= arr[i-1]:
+                return False
+        return True
