1e13da883fcd3259cd578e537f0c7307485c8a92
Eklavaya Singh <eklavya.singh7933@gmail.com>
2026-06-15 23:19:11 +0530

    BST question

---
+++ Binary search Tree/Search in a Binary Search Tree.py
@@ -0,0 +1,16 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        temp = root
+        while temp is not None:
+            if temp.val == val:
+                return temp
+            elif val < temp.val:
+                temp = temp.left
+            else:
+                temp = temp.right
+        return None
