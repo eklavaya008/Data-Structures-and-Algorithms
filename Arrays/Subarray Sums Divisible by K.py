5b83e28b820ec2efb2580afbe54c44d81a11e65a
Eklavaya Singh <eklavya.singh7933@gmail.com>
2026-08-07 22:46:51 +0530

    array question

---
+++ Arrays/Subarray Sums Divisible by K.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def subarraysDivByK(self, nums, k):
+        remainder_count = {0:1}
+        prefix_sum = 0
+        count = 0
+
+        for num in nums:
+            prefix_sum += num
+            remainder = prefix_sum % k
+
+            if (remainder) in remainder_count:
+                count += remainder_count[remainder]
+            
+            remainder_count[remainder] = remainder_count.get(remainder,0) +1
+        return count
